6dbf5dc88a7b18f4b8344a70a96f06c1337b9965
Sandro Campos <scampos80@live.com>
2020-02-22 17:17:33 -0600

    Added the root route, index and show for films, characters, planets and races.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :films, only: [:index, :show]
+  # get "/films", to: 'films#index'
+  # get '/films/:id', to: 'films#show', id: /\d/
+
+  resources :characters, only: [:index, :show]
+  # get '/characters/:id', to: 'characters#show', id: /\d/
+
+  resources :planets, only: [:index, :show]
+  # get '/planets/:id', to: 'planets#show', id: /\d/
+
+  resources :races, only: [:index, :show]
+  # get '/races/:id', to: 'races#show', id: /\d/
+
+  root to: 'films#index'
+  # get '/characters', to: 'characters#index'
+  # get '/planets', to: 'planets#index'
+  # get '/races', to: 'races#index'
 end
