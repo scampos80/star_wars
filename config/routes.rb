d5f14309abf342490107b5c10d6d2ab386fe1827
Sandro Campos <scampos80@live.com>
2020-02-26 15:46:58 -0600

    Created About page and its route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,26 @@
 Rails.application.routes.draw do
 
-  resources :pages
-  get '/static/:permalink', to: 'pages#permalink', as: 'permalink'
-
   resources :films, only: [:index, :show]
   # get "/films", to: 'films#index'
   # get '/films/:id', to: 'films#show', id: /\d/
 
-  resources :characters, only: [:index, :show]
+  resources :characters, only: [:index, :show] do
+    collection do
+      get 'c_search_results'
+    end
+  end
   # get '/characters/:id', to: 'characters#show', id: /\d/
+  # get '/characters', to: 'characters#index'
 
   resources :planets, only: [:index, :show]
   # get '/planets/:id', to: 'planets#show', id: /\d/
+  # get '/planets', to: 'planets#index'
 
   resources :races, only: [:index, :show]
   # get '/races/:id', to: 'races#show', id: /\d/
+  # get '/races', to: 'races#index'
+
+  get 'about', to: 'pages#about', as: 'about'
 
   root to: 'films#index'
-  # get '/characters', to: 'characters#index'
-  # get '/planets', to: 'planets#index'
-  # get '/races', to: 'races#index'
 end
